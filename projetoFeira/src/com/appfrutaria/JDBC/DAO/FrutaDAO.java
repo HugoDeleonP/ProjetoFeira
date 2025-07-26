41af33233cf2024a803c45abf6b57ef12e00e3b5
Hugo <hugo_paim@estudante.sesisenai.org.br>
2025-07-26 16:03:52 -0300

    feat: acrescentar métodos essenciais a FrutaDAO e VerduraDAO.

--- projetoFeira/src/com/appfrutaria/JDBC/DAO/FrutaDAO.java
+++ projetoFeira/src/com/appfrutaria/JDBC/DAO/FrutaDAO.java
@@ -1,4 +1,89 @@
 package com.appfrutaria.JDBC.DAO;
 
+import com.appfrutaria.JDBC.Conexao;
+import com.appfrutaria.model.Produto;
+import com.appfrutaria.view.Atendente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class FrutaDAO {
+
+    public void inserir(ProdutoDAO produtoDAO, Atendente atendente){
+
+        int idProduto = produtoDAO.inserir(atendente);
+
+        try{
+            Connection conn = Conexao.getConnection();
+
+            double peso = atendente.writePeso();
+            String sql = "INSERT INTO fruta (id_produto, peso) VALUES (?, ?)";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, idProduto);
+            stmt.setDouble(2, peso);
+
+            stmt.executeQuery();
+            atendente.operacaoRealizada();
+
+            stmt.close();
+            conn.close();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void listar(Atendente atendente){
+        try{
+            Connection conn = Conexao.getConnection();
+
+            String sql = "SELECT * FROM fruta";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                atendente.showID();
+                System.out.println(rs.getInt("id_produto"));
+
+                atendente.showPeso();
+                System.out.println(rs.getDouble("peso"));
+            }
+            else {
+                System.out.println("Erro");
+            }
+
+            stmt.close();
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void remover(Atendente atendente){
+        try{
+            Connection conn = Conexao.getConnection();
+
+            String sql = "DELETE FROM fruta WHERE id_produto = ?";
+
+            int id = atendente.writeID();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+            atendente.operacaoRealizada();
+            stmt.close();
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- projetoFeira/src/com/appfrutaria/JDBC/DAO/VerduraDAO.java
+++ projetoFeira/src/com/appfrutaria/JDBC/DAO/VerduraDAO.java
@@ -1,4 +1,87 @@
 package com.appfrutaria.JDBC.DAO;
 
+import com.appfrutaria.JDBC.Conexao;
+import com.appfrutaria.view.Atendente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class VerduraDAO {
+    public void inserir(ProdutoDAO produtoDAO, Atendente atendente){
+
+        int idProduto = produtoDAO.inserir(atendente);
+
+        try{
+            Connection conn = Conexao.getConnection();
+
+            String tipo = atendente.writeTipo();
+            String sql = "INSERT INTO verdura (id_produto, tipo) VALUES (?, ?)";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, idProduto);
+            stmt.setString(2, tipo);
+
+            stmt.executeQuery();
+            atendente.operacaoRealizada();
+
+            stmt.close();
+            conn.close();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void listar(Atendente atendente){
+        try{
+            Connection conn = Conexao.getConnection();
+
+            String sql = "SELECT * FROM verdura";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            if(rs.next()){
+                atendente.showID();
+                System.out.println(rs.getInt("id_produto"));
+
+                atendente.showPeso();
+                System.out.println(rs.getString("tipo"));
+            }
+            else {
+                System.out.println("Erro");
+            }
+
+            stmt.close();
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void remover(Atendente atendente){
+        try{
+            Connection conn = Conexao.getConnection();
+
+            String sql = "DELETE FROM verdura WHERE id_produto = ?";
+
+            int id = atendente.writeID();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+            atendente.operacaoRealizada();
+            stmt.close();
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
